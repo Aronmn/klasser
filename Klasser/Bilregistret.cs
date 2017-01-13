36b87a5ca063017277ed824e1e9aed0a936aed05
Aron <aronmn@vgy.se>
2017-01-13 15:00:52 +0100

    Övning 1 - Övning 2

---
+++ Klasser/Bilregistret.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klasser
+{
+    class Bilregistret
+    {
+        public string Marke { get; set; }
+        public string Modell { get; set; }
+        public string Farg { get; set; }
+        public string Dorrar { get; set; }
+        public string Drivmedel { get; set; }
+    }
+}
--- Klasser/Program.cs
+++ Klasser/Program.cs
@@ -10,6 +10,28 @@ namespace Klasser
     {
         static void Main(string[] args)
         {
+            //Övning 1
+
+            Bilregistret info = new Bilregistret();
+
+            Console.Write("Ange bilmärke: ");
+            info.Marke = Console.ReadLine();
+            Console.Write("Ange modell: ");
+            info.Modell = Console.ReadLine();
+            Console.Write("Ange färg: ");
+            info.Farg = Console.ReadLine();
+            Console.Write("Ange antal dörrar: ");
+            info.Dorrar = Console.ReadLine();
+            Console.Write("Ange drivmedel: ");
+            info.Drivmedel = Console.ReadLine();
+
+            Console.WriteLine("Din bil är en " + info.Marke +" "+ info.Modell + " som är lackerad i " + info.Farg + " och har " + info.Dorrar + " dörrar. Drivmedlet är " + info.Drivmedel);
+
+            Console.ReadKey();
+
+            //Övning 2
+
+
         }
     }
 }
